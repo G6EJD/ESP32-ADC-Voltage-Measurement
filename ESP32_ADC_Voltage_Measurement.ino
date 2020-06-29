1ef8e7a312b3b186821468f7beceb26f78b381ea
G6EJD <d.l.bird@sky.com>
2020-06-29 22:04:11 +0100

    Add files via upload

---
+++ ESP32_ADC_Voltage_Measurement.ino
@@ -0,0 +1,24 @@
+// 0 - 5 volts range Example
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println(readVoltage(),1);
+  float BatteryVoltage = readVoltage();
+  delay(1000);
+}
+
+float readVoltage(){
+  #define constant 5.9; // 0-5v range example
+  // #define constant 7.1;   // 0-6v   range
+  // #define constant 14.6;  // 0-12v  range
+  // #define constant 18.5;  // 0-15v  range
+  // #define constant 30.1;  // 0-24v  range
+  // #define constant 38.1;  // 0-30v  range
+  // #define constant 64.5;  // 0-50v  range
+  // #define constant 130.0; // 0-100v range
+  float ADCvoltage = (float)analogRead(36) / 4095 * constant;
+  return ADCvoltage * constant; // 0-5v   range
+}
